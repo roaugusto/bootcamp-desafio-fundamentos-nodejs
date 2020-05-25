6d349addc9f24a600a85c11105790fae1c68e464
Rodrigo Santos <ro.augusto@gmail.com>
2020-05-25 13:34:05 -0300

    Implementing list transactions and getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const outcome = this.transactions
+      .filter(item => item.type === 'outcome')
+      .reduce<number>((sum, item) => {
+        return sum + item.value;
+      }, 0);
+
+    const income = this.transactions
+      .filter(item => item.type === 'income')
+      .reduce<number>((sum, item) => {
+        return sum + item.value;
+      }, 0);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
